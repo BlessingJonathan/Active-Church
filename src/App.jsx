ce0ceab1698b8bc06a003637b0d8a0d5ba6a3e36
Blessing <blessingjonathan028@gmail.com>
2025-09-30 06:29:40 +0200

    Fixed imports

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import HomePage from './pages/HomePage'
 import { Routes, Route } from 'react-router-dom'
 import GivePage from './pages/GivePage'
 import Watch_Live from './pages/Watch_Live'
-import Contact_Us from './Pages/Contact_Us'
+import Contact_Us from './pages/Contact_Us'
 import Navbar from './components/Navbar'
-import Vision from './Pages/Vision'
+import Vision from './pages/Vision'
 import Footer from './components/Footer'
 
 
